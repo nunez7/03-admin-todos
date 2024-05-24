c7f11c57330c084b8ed3ef3414dd5f81030589ef
Felix Javier Nunez Gonzalez <nem.fnunez@coppel.com>
2024-05-23 18:13:27 -0700

    Estructura y diseño de TODOs

--- src/app/dashboard/rest-todos/page.tsx
+++ src/app/dashboard/rest-todos/page.tsx
@@ -1,8 +1,25 @@
+import prisma from "@/lib/prisma";
+import { TodosGrid } from "@/todos";
 
-export default function RestTodosPage() {
+export const metadata = {
+ title: 'Listado de TODOs',
+ description: 'Listado de TODOs',
+};
+
+export default async function RestTodosPage() {
+
+  /*useEffect(() => {
+    fetch('localhost:3000/api/todos/')
+    .then(resp => resp.json())
+    .then(console.log);
+  }, [])*/
+
+  const todos = await prisma.todo.findMany({orderBy: {description: 'asc'}});
+
   return (
     <div>
-      <h1>RestTodosPage</h1>
+      {/* FORM para agregar TODOs */}
+      <TodosGrid todos={todos}/>
     </div>
   );
 }
