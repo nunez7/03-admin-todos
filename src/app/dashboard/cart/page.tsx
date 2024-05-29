a3ce2c4e9fb3541d08ad041893ac78c56c215a69
Felix Javier Nunez Gonzalez <nem.fnunez@coppel.com>
2024-05-29 11:37:43 -0700

    Total + impuesto

--- src/app/dashboard/cart/page.tsx
+++ src/app/dashboard/cart/page.tsx
@@ -1,3 +1,4 @@
+import { WidgetItem } from "@/components";
 import { products, type Product } from "@/products/data/products";
 import { ItemCard } from "@/shopping-cart";
 import { cookies } from "next/headers";
@@ -29,10 +30,11 @@ const getProductsInCart = (cart: {[id:string]: number}): ProductsInCart[] =>{
 export default function CartPage() {
 
     const cookiesStore = cookies();
-
     const cart = JSON.parse(cookiesStore.get('cart')?.value ?? '{}') as {[id:string]: number};
-
     const productsInCart = getProductsInCart(cart);
+
+    const totalToPay = productsInCart.reduce((prev, current) => 
+        (current.product.price * current.quantity) + prev, 0 );
 
   return (
     <div>
@@ -45,6 +47,15 @@ export default function CartPage() {
                 <ItemCard key={product.id} product={product} quantity={quantity} />
              ))
             }
+        </div>
+
+        <div className="flex flex-col w-full sm:w-4/12">
+            <WidgetItem title="Total a pagar">
+                <div className="mt-2 flex justify-center gap-4">
+                    <h3 className="text-3xl font-bold text-gray-700">$ {(totalToPay * 1.16).toFixed(2)}</h3>
+                </div>
+                <span className="font-bold text-center text-gray-500">Impuestos 16%: $ {(totalToPay * 0.16).toFixed(2)}</span>
+            </WidgetItem>
         </div>
       </div>
     </div>
