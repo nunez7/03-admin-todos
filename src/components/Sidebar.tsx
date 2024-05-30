e90a29dc1027905b9a99aa9b6db4d0aef0c411cc
Felix Javier Nunez Gonzalez <nem.fnunez@coppel.com>
2024-05-30 14:28:20 -0700

    Informacion del usuario

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,11 @@
 import Image from 'next/image';
 import React from 'react';
+import { getServerSession } from "next-auth";
 import { CiBookmarkCheck, CiLogout } from 'react-icons/ci';
 import { SidebarItem } from './SidebarItem';
 import { IoBasketballOutline, IoCalendarOutline, IoCheckboxOutline, IoCodeWorkingOutline, IoListOutline } from 'react-icons/io5';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { redirect } from 'next/navigation';
 
 const sidebarItems = [
     {
@@ -32,7 +35,19 @@ const sidebarItems = [
       },
 ];
 
-export const Sidebar = () => {
+export const Sidebar = async() => {
+  const session = await getServerSession(authOptions);
+
+  if(!session){
+    redirect('/api/auth/signin');
+  }
+
+  const userName = session.user?.name;
+  const userImage = (session.user?.image ) 
+  ? session.user?.image
+  : 'https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp';
+  const userRole = (session.user?.name ) ? session.user?.name : 'Admin';
+
   return (
     <aside className="ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
         <div>
@@ -44,11 +59,11 @@ export const Sidebar = () => {
           </div>
 
           <div className="mt-8 text-center">
-            <Image src="https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp"
+            <Image src={userImage}
              alt="" className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28"
              width={150} height={150}/>
-              <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">Cynthia J. Watts</h5>
-              <span className="hidden text-gray-400 lg:block">Admin</span>
+              <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">{userName}</h5>
+              <span className="hidden text-gray-400 lg:block">{userRole}</span>
           </div>
 
           <ul className="space-y-2 tracking-wide mt-8">
