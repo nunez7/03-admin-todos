ef2597011d6d44a969a1761ccb7ef2cdf382d715
Felix Javier Nunez Gonzalez <nem.fnunez@coppel.com>
2024-05-31 13:29:28 -0700

    Proveedor de credenciales, termina logeo

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,7 +4,8 @@ import NextAuth, { NextAuthOptions } from "next-auth";
 import { Adapter } from "next-auth/adapters";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
-import async from '../../../dashboard/page';
+import CredentialsProvider from "next-auth/providers/credentials";
+import { signInEmailPassword } from "@/actions/auth-actions";
 
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
@@ -17,6 +18,22 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ''
+    }),
+
+    CredentialsProvider({
+      credentials: {
+        email: { label: "Correo electrónico", type: "email", placeholder: "usuario@gmail.com" },
+        password: { label: "Contraseña", type: "password" , placeholder: "******"}
+      },
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        const user = await signInEmailPassword(credentials!.email, credentials!.password);
+  
+        if (user) {
+          return user;
+        } 
+        return null;
+      }
     })
   ],
 
